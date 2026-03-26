1a7b9c21592e592fa9015634db7cf329bb7ec1f8
Alexey Shibanov <79175825855@yandex.ru>
2026-03-26 22:42:07 +0300

    feat: added calculate and output bmi

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bmi-go
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const IMT_POWER = 2
+
+func main() {
+	userWeight, userHeight := getUserValues()
+
+	if userHeight == 0 || userWeight == 0 {
+		fmt.Println("Invalid values. Please enter correct your parameters.")
+		return
+	}
+
+	bmi := calculateBmi(userWeight, userHeight)
+	outputResultBmi(bmi)
+}
+
+func outputResultBmi(bmi float64) {
+	fmt.Printf("Your BMI: %.0f\n", bmi)
+	fmt.Println("Category:", getBmiCategory(bmi))
+}
+
+func calculateBmi(userWeight, userHeight float64) float64 {
+	// рост вводится в сантиметрах → переводим в метры
+	heightMeters := userHeight / 100
+	result := userWeight / math.Pow(heightMeters, IMT_POWER)
+	return result
+}
+
+func getUserValues() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+
+	fmt.Print("Your height (cm): ")
+	fmt.Scan(&userHeight)
+
+	fmt.Print("Your weight (kg): ")
+	fmt.Scan(&userWeight)
+
+	return userWeight, userHeight
+}
+
+func getBmiCategory(bmi float64) string {
+	switch {
+	case bmi < 18.5:
+		return "Underweight"
+	case bmi < 25:
+		return "Normal"
+	case bmi < 30:
+		return "Overweight"
+	default:
+		return "Obese"
+	}
+}
